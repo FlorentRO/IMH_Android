44547902ee4db5306725359365a096741717f51e
FlorentRO <florent.robert@etu.unice.fr>
2019-04-28 20:43:18 +0200

    AmiTest

--- app/src/main/java/fr/etudes/redugaspi/fragments/TabFragment3.java
+++ app/src/main/java/fr/etudes/redugaspi/fragments/TabFragment3.java
@@ -1,7 +1,10 @@
 package fr.etudes.redugaspi.fragments;
 
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -9,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import fr.etudes.redugaspi.R;
@@ -32,7 +36,23 @@ public class TabFragment3 extends Fragment implements IListenItem {
                                                             Kindergarten.getListTwo() );
 */
         IndianAdapter indiansAdapter = new IndianAdapter(getContext(), Kindergarten.getListTwo());
+        Button callButton = view.findViewById(R.id.callButton);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "0606060606", null));
+                startActivity(callIntent);
+            }});
 
+        Button mailButton = view.findViewById(R.id.mailButton);
+        mailButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:" + "0606060606" + "?subject=" + "Salut" + "&body=" + "");
+                emailIntent.setData(data);
+                startActivity(emailIntent);
+            }
+        });
 
         //get ListView indians and cowboys
 
