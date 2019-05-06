12278e8f9df6820bd4db07f40f5944cedc306196
FlorentRO <florent.robert@etu.unice.fr>
2019-05-06 15:47:41 +0200

    Connexion

--- app/src/main/java/fr/etudes/redugaspi/activities/MainActivity.java
+++ app/src/main/java/fr/etudes/redugaspi/activities/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import fr.etudes.redugaspi.R;
 
 
@@ -16,12 +19,24 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button valid = findViewById(R.id.ok);
+        final EditText id = findViewById(R.id.id);
+        final EditText mdp = findViewById(R.id.mdp);
+        final TextView error = findViewById(R.id.error);
 
         valid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ManagementActivity.class);
-                startActivity(intent);
+                System.out.println(id.getText());
+                System.out.println(mdp.getText());
+                System.out.println(id.getText().equals("mdp"));
+                if ( (id.getText().toString().equals("corinne") || id.getText().toString().equals("bertrand") )
+                        && mdp.getText().toString().equals("mdp") ) {
+                    error.setVisibility(View.INVISIBLE);
+                    Intent intent = new Intent(getApplicationContext(), ManagementActivity.class);
+                    startActivity(intent);
+                } else {
+                    error.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
