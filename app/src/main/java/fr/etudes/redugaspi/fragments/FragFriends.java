1b23f0e4ffe9517bc446dccd9875d4152aa34380
FlorentRO <florent.robert@etu.unice.fr>
2019-05-06 16:41:25 +0200

    AjoutAmi_NonFonctionnel

--- app/src/main/java/fr/etudes/redugaspi/fragments/FragFriends.java
+++ app/src/main/java/fr/etudes/redugaspi/fragments/FragFriends.java
@@ -1,6 +1,7 @@
 package fr.etudes.redugaspi.fragments;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,25 +24,25 @@ import fr.etudes.redugaspi.adapters.FriendAdapter;
 import fr.etudes.redugaspi.models.User;
 
 public class FragFriends extends Fragment implements IListenItem {
+    List<User> users = new ArrayList<User>();
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.frag_friends, container, false);
-        List<User> users = new ArrayList<User>();
-                users.add(new User("Corinne Renardo",0606060600));
-                users.add(new User("Bertrand Dupont",0706060600));
-                users.add(new User("Florent Kinowa",0606060600));
-                users.add(new User("Aldric Neogeekmo",0606060600));
-                users.add(new User("Couette Inaccoutumé",0006060600));
-                users.add(new User("William Daubard",0006060600));
-                users.add(new User("Aelita Stone",0006060600));
-                users.add(new User("Harry Potter",0006060600));
-                users.add(new User("Voajaje Travajer",0006060600));
-                users.add(new User("Jaiplus Didée",0006060600));
-                users.add(new User("Encore Undernier",0006060600));
 
+        //Liste d'ami de base
+        users.add(new User("Corinne Renardo",0606060600));
+        users.add(new User("Bertrand Dupont",0706060600));
+        users.add(new User("Florent Kinowa",0606060600));
+        users.add(new User("Aldric Neogeekmo",0606060600));
+        users.add(new User("Couette Inaccoutumé",0006060600));
+        users.add(new User("William Daubard",0006060600));
+        users.add(new User("Aelita Stone",0006060600));
+        users.add(new User("Harry Potter",0006060600));
+        users.add(new User("Voajaje Travajer",0006060600));
+        users.add(new User("Jaiplus Didée",0006060600));
+        users.add(new User("Encore Undernier",0006060600));
 
-        FriendAdapter friendAdapter= new FriendAdapter(getContext(), users.stream().map(User::getPseudo).collect(Collectors.toList()));
-
+        //Add un ami
         Button btnAddFriend = view.findViewById(R.id.btn_add_friend);
         btnAddFriend.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(FragFriends.this.getContext());
@@ -51,17 +52,20 @@ public class FragFriends extends Fragment implements IListenItem {
             builder.setTitle("Ajouter un ami");
             builder.setMessage("Qui voulez vous ajouter ?");
             builder.setNeutralButton("Annuler", null);
-            builder.setPositiveButton("Ajouter", null);
+            builder.setPositiveButton("Ajouter", (dialog, id) -> {
+                addAmi(input.toString());
+            });
             builder.show();
         });
 
+        //Liste ami dans la liste
+        FriendAdapter friendAdapter= new FriendAdapter(getContext(), users.stream().map(User::getPseudo).collect(Collectors.toList()));
         EditText searchText = view.findViewById(R.id.txt_search_friend);
         ListView friendsListView = view.findViewById(R.id.lst_friends);
-
         friendsListView.setAdapter(friendAdapter);
         friendsListView.setTextFilterEnabled(true);
-
         friendAdapter.addListener(this);
+
 
         searchText.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) { }
@@ -85,5 +89,9 @@ public class FragFriends extends Fragment implements IListenItem {
         builder.setNegativeButton("SMS",
                 (dialog, which) -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", "0606060606", null))));
         builder.show();
+    }
+
+    public void addAmi(String input){
+        users.add(new User(input,0606060600));
     }
 }
