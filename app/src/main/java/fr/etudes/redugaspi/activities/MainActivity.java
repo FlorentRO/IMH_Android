fcdc746af097b9f42ad17a790e24b96f20a8d63e
Theo-Fafet <theo.fafet@etu.unice.fr>
2019-05-08 19:19:09 +0200

    Fix layouts

--- app/src/main/java/fr/etudes/redugaspi/activities/MainActivity.java
+++ app/src/main/java/fr/etudes/redugaspi/activities/MainActivity.java
@@ -4,25 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import fr.etudes.redugaspi.R;
 
 
 public class MainActivity extends AppCompatActivity{
-    private static int step = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button valid = findViewById(R.id.ok);
-        final EditText id = findViewById(R.id.id);
-        final EditText mdp = findViewById(R.id.mdp);
-        final TextView error = findViewById(R.id.error);
+        Button valid = findViewById(R.id.connect);
 
         valid.setOnClickListener(v -> {
             Intent intent = new Intent(getApplicationContext(), ManagementActivity.class);
